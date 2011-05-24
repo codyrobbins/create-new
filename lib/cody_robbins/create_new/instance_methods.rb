50d315781803fed36f4e3e951a1430bd913a54a4
Cody Robbins <cody@sakuzaku.com>
2011-05-24 17:56:59 -0400

    Added some documentation.

--- lib/cody_robbins/create_new/instance_methods.rb
+++ lib/cody_robbins/create_new/instance_methods.rb
@@ -1,6 +1,28 @@
 module CodyRobbins
   module CreateNew
     module InstanceMethods
+      # Creates a new instance of an ActiveRecord model and assigns it to an instance variable.
+      #
+      # * You pass the name of the model to create to the method.
+      # * The key in `params` assumed to contain the attributes of the model is extrapolated from the model name. For example, if the model is `User` then this key would be `:user`.
+      # * The instance variable that the new instance is assigned to will be named according to the model. For example, if the model is `User` then the instance variable will be `@user`.
+      #
+      # @param name [Symbol, String] The name of the model to create.
+      # @return Returns the new instance of the model.
+      #
+      # @example The following two actions are equivalent.
+      #
+      #     class UserController < ApplicationController
+      #       # The usual Rails way.
+      #       def create
+      #         @user = User.new(params[:user])
+      #       end
+      #
+      #       # Using create-new.
+      #       def create
+      #         create_new(:user)
+      #       end
+      #     end
       def create_new(name)
         model = name.to_class
         attributes = params[name]
